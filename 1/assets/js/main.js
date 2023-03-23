59dbec0253f1d1f24df03164f9098dfa38c6df07
giuseppesciacca <sciaccagi@gmail.com>
2023-03-23 11:40:37 +0100

    [add] cars array

---
+++ 1/assets/js/main.js
@@ -0,0 +1,64 @@
+/* Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+ 
+Infine stampa separatamente i 3 array. */
+
+/* Crea un array composto da 10 automobili. 
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano). */
+const cars = [
+    {
+        brand: 'fiat',
+        model: 'panda',
+        power: 'metano'
+    },
+    {
+        brand: 'lancia',
+        model: 'ypsilon',
+        power: 'benzina'
+    },
+    {
+        brand: 'fiat',
+        model: 'tipo',
+        power: 'diesel'
+    },
+    {
+        brand: 'fiat',
+        model: '500X',
+        power: 'diesel'
+    },
+    {
+        brand: 'tesla',
+        model: 'model 3',
+        power: 'elettrica'
+    },
+    {
+        brand: 'dacia',
+        model: 'spring',
+        power: 'elettrica'
+    },
+    {
+        brand: 'tesla',
+        model: 'model s',
+        power: 'elettrica'
+    },
+    {
+        brand: 'volkswagen',
+        model: 'polo',
+        power: 'metano'
+    },
+    {
+        brand: 'dacia',
+        model: 'sandero stepway',
+        power: 'benzina'
+    },
+    {
+        brand: 'dacia',
+        model: 'logan',
+        power: 'gpl'
+    },
+]
+
+/* Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto. */
+
